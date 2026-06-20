c07c9dd8e62c1f05bbc8f49512bd673bd9d37003
samsit-phew <samsitkarki@gmail.com>
2026-06-20 12:19:21 +0545

    0.0.0

--- src/main.zig
+++ src/main.zig
@@ -2,6 +2,9 @@ const std = @import("std");
 const b64 = @import("base64ed");
 
 pub fn main(init: std.process.Init) !void {
+    var memBuff: [1024]u8 = undefined;
+    var fba = std.heap.FixedBufferAllocator.init(&memBuff);
+    const alloc = fba.allocator();
     errdefer std.debug.print("opps something went wrong", .{});
     const ed = b64.Base64.init();
     const ar = init.arena.allocator();
@@ -13,10 +16,10 @@ pub fn main(init: std.process.Init) !void {
     const cmd = args[1];
     const text = args[2];
     if (std.ascii.eqlIgnoreCase(cmd, "encode")) {
-        const out = ed.encode(init.gpa, text);
+        const out = try ed.encode(alloc, text);
         std.debug.print("{s} = {s}", .{ text, out });
     } else if (std.ascii.eqlIgnoreCase(cmd, "decode")) {
-        const out = ed.decode(init.gpa, "text");
+        const out = try ed.decode(alloc, "text");
         std.debug.print("{s} = {s}", .{ text, out });
     }
 }
